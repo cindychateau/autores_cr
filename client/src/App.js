f82550e054f7e7c474f7f8f714f5df5985b825f1
Cynthia Castillo <ca.castillo@gmail.com>
2023-09-06 21:58:27 -0600

    Vista Autores y Nuevo Autor. Backend ver, editar y borrar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import {Routes, Route} from 'react-router-dom';
+import TodosAutores from './componentes/TodosAutores';
+import NuevoAutor from './componentes/NuevoAutor';
 
 const App = () => {
   return(
     <div className="container">
       <h1>Autores</h1>
+      <Routes>
+        <Route path="/" exact element={<TodosAutores />} />
+        <Route path="/nuevo" element={<NuevoAutor />} />
+      </Routes>
     </div>
   )
 }
--- server/controllers/autor.controller.js
+++ server/controllers/autor.controller.js
@@ -15,3 +15,29 @@ module.exports.create_autor = (req, res) => {
             res.status(400).json(err);
         });
 }
+
+module.exports.get_autor = (req, res) => {
+    //req.params.id = 123456
+    Autor.findOne({_id: req.params.id})
+        .then(autor => res.json(autor))
+        .catch(err =>{
+            res.status(400).json(err);
+        });
+}
+
+module.exports.update_autor = (req, res) => {
+    //req.params.id = 123456
+    Autor.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators:true})
+        .then(autor => res.json(autor))
+        .catch(err =>{
+            res.status(400).json(err);
+        });
+}
+
+module.exports.delete_autor = (req, res) => {
+    Autor.deleteOne({_id: req.params.id})
+        .then(result => res.json(result))
+        .catch(err =>{
+            res.status(400).json(err);
+        });
+}
